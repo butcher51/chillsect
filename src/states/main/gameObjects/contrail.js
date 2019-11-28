e5bd49cda747c1b5bf127967367dea51c12a619d
butcher51 <gabor.meszaros@gmail.com>
2019-11-28 21:56:21 +0100

    three111

--- src/states/main/gameObjects/contrail.js
+++ src/states/main/gameObjects/contrail.js
@@ -1,126 +1,111 @@
-import {
-    Mesh,
-    AdditiveBlending,
-    Vector3,
-    PlaneBufferGeometry,
-    DoubleSide,
-    BufferAttribute,
-    MeshBasicMaterial,
-    ShaderMaterial,
-    Vector2
-} from "three";
-import {vec2} from "p2";
+import { Mesh, AdditiveBlending, Vector3, PlaneBufferGeometry, DoubleSide, BufferAttribute, MeshBasicMaterial, ShaderMaterial, Vector2 } from "three";
+import { vec2 } from "p2";
 
-var Contrail = function (engine, texture, width = 1, thrust = 0.3) {
+var Contrail = function(engine, texture, width = 1, thrust = 0.3) {
+  this.engine = engine;
 
-    this.engine = engine;
+  var geometry = new PlaneBufferGeometry(1, 1, parseInt(this.length / this.break) - 1, 1);
 
-    var geometry = new PlaneBufferGeometry(1, 1, parseInt(this.length / this.break) - 1, 1);
+  var vertexshader = [
+    "uniform vec2 objectPosition;",
+    "uniform vec2 objectVelocity;",
+    "uniform float objectRotation;",
+    "uniform float objectRoll;",
 
-    var vertexshader = [
+    "uniform float length;",
 
-        'uniform vec2 objectPosition;',
-        'uniform vec2 objectVelocity;',
-        'uniform float objectRotation;',
-        'uniform float objectRoll;',
+    "attribute vec3 objectPositions;",
+    "attribute vec2 objectVelocities;",
+    "attribute float objectRotations;",
+    "attribute float objectRolls;",
 
-        'uniform float length;',
+    "varying vec2 vUv;",
 
-        'attribute vec3 objectPositions;',
-        'attribute vec2 objectVelocities;',
-        'attribute float objectRotations;',
-        'attribute float objectRolls;',
+    "mat3 rotateZ(float rad) {",
+    "   float c = cos(rad);",
+    "   float s = sin(rad);",
+    "   return mat3(",
+    "       c, s, 0.0,",
+    "       -s, c, 0.0,",
+    "       0.0, 0.0, 1.0",
+    "   );",
+    "}",
 
-        'varying vec2 vUv;',
+    "mat3 rotateX(float rad) {",
+    "   float c = cos(rad);",
+    "   float s = sin(rad);",
+    "   return mat3(",
+    "      1.0, 0.0, 0.0,",
+    "      0.0, c, s,",
+    "      0.0, -s, c",
+    "   );",
+    "}",
 
-        'mat3 rotateZ(float rad) {',
-        '   float c = cos(rad);',
-        '   float s = sin(rad);',
-        '   return mat3(',
-        '       c, s, 0.0,',
-        '       -s, c, 0.0,',
-        '       0.0, 0.0, 1.0',
-        '   );',
-        '}',
+    "void main() {",
 
-        'mat3 rotateX(float rad) {',
-        '   float c = cos(rad);',
-        '   float s = sin(rad);',
-        '   return mat3(',
-        '      1.0, 0.0, 0.0,',
-        '      0.0, c, s,',
-        '      0.0, -s, c',
-        '   );',
-        '}',
+    "vUv=uv;",
 
-        'void main() {',
+    "vec3 pointVector = vec3(objectPositions.x - objectPosition.x ,objectPositions.y - objectPosition.y, 0);",
+    "pointVector = pointVector * rotateZ(objectRotation);",
 
-        'vUv=uv;',
+    "vec3 velocityVector = vec3(objectVelocities.x - objectVelocity.x,objectVelocities.y - objectVelocity.y , 0);",
+    "velocityVector = velocityVector * rotateZ(objectRotation) * " + thrust + ";",
 
-        'vec3 pointVector = vec3(objectPositions.x - objectPosition.x ,objectPositions.y - objectPosition.y, 0);',
-        'pointVector = pointVector * rotateZ(objectRotation);',
+    "vec3 vertex = vec3(0.0,objectPositions.z * " + width + ".0,0.0) * rotateZ(objectRotation-objectRotations) * rotateX(-objectRoll) ;",
 
-        'vec3 velocityVector = vec3(objectVelocities.x - objectVelocity.x,objectVelocities.y - objectVelocity.y , 0);',
-        'velocityVector = velocityVector * rotateZ(objectRotation) * ' + thrust + ';',
+    "vec4 mvPosition = modelViewMatrix * vec4( vertex.xyz + pointVector.xyz + velocityVector.xyz, 1.0 );",
 
-        'vec3 vertex = vec3(0.0,objectPositions.z * ' + width + '.0,0.0) * rotateZ(objectRotation-objectRotations) * rotateX(-objectRoll) ;',
+    "gl_Position = projectionMatrix * mvPosition;",
 
-        'vec4 mvPosition = modelViewMatrix * vec4( vertex.xyz + pointVector.xyz + velocityVector.xyz, 1.0 );',
+    "}",
 
-        'gl_Position = projectionMatrix * mvPosition;',
+    ""
+  ].join("\n");
 
-        '}',
+  var fragmentshader = [
+    "uniform sampler2D texture;",
+    "varying vec2 vUv;",
 
-        ''].join('\n');
+    "void main() {",
 
+    "gl_FragColor = texture2D( texture, vUv);",
+    // 'gl_FragColor = vec4(1.0,0,0,1.0);',
+    "}"
+  ].join("\n");
 
-    var fragmentshader = [
+  var material = new ShaderMaterial({
+    uniforms: {
+      texture: { type: "t", value: texture },
+      length: { type: "f", value: this.length },
+      objectRotation: { type: "f", value: 0 },
+      objectRoll: { type: "f", value: 0 },
+      objectPosition: { type: "v2", value: new Vector2(0, 0) },
+      objectVelocity: { type: "v2", value: new Vector2(0, 0) }
+    },
+    vertexShader: vertexshader,
+    fragmentShader: fragmentshader,
+    transparent: true,
+    depthTest: true,
+    blending: AdditiveBlending,
+    depthWrite: false,
+    wireframe: false
+  });
 
-        'uniform sampler2D texture;',
-        'varying vec2 vUv;',
+  geometry.setAttribute("objectPositions", new BufferAttribute(new Float32Array(geometry.attributes.position.count * 3), 3));
+  geometry.setAttribute("objectVelocities", new BufferAttribute(new Float32Array(geometry.attributes.position.count * 3), 3));
+  geometry.setAttribute("objectRotations", new BufferAttribute(new Float32Array(geometry.attributes.position.count * 1), 1));
+  geometry.setAttribute("objectRolls", new BufferAttribute(new Float32Array(geometry.attributes.position.count * 1), 1));
 
-        'void main() {',
+  Mesh.call(this, geometry, material);
 
-        'gl_FragColor = texture2D( texture, vUv);',
-        // 'gl_FragColor = vec4(1.0,0,0,1.0);',
-        '}'
+  this.frustumCulled = false;
 
-    ].join('\n');
+  this.positionHistory = [];
+  for (var i = this.length; i > 0; i--) {
+    this.positionHistory[this.length - i] = [0, 0, 0, 0, 0, 0];
+  }
 
-
-    var material = new ShaderMaterial({
-        uniforms: {
-            texture: {type: "t", value: texture},
-            length: {type: 'f', value: this.length},
-            objectRotation: {type: 'f', value: 0},
-            objectRoll: {type: 'f', value: 0},
-            objectPosition: {type: 'v2', value: new Vector2(0, 0)},
-            objectVelocity: {type: 'v2', value: new Vector2(0, 0)},
-        },
-        vertexShader: vertexshader,
-        fragmentShader: fragmentshader,
-        transparent: true,
-        depthTest: true,
-        blending: AdditiveBlending,
-        depthWrite: false,
-        wireframe: false
-    });
-
-    geometry.addAttribute('objectPositions', new BufferAttribute(new Float32Array(geometry.attributes.position.count * 3), 3));
-    geometry.addAttribute('objectVelocities', new BufferAttribute(new Float32Array(geometry.attributes.position.count * 3), 3));
-    geometry.addAttribute('objectRotations', new BufferAttribute(new Float32Array(geometry.attributes.position.count * 1), 1));
-    geometry.addAttribute('objectRolls', new BufferAttribute(new Float32Array(geometry.attributes.position.count * 1), 1));
-
-    Mesh.call(this, geometry, material);
-
-    this.frustumCulled = false;
-
-    this.positionHistory = [];
-    for (var i = this.length; i > 0; i--) {
-        this.positionHistory[this.length - i] = [0, 0, 0, 0, 0, 0];
-    }
-
-    this.renderOrder = -2;
+  this.renderOrder = -2;
 };
 
 Contrail.prototype = Object.create(Mesh.prototype);
@@ -130,75 +115,70 @@ Contrail.prototype.break = 5;
 Contrail.prototype.length = 50;
 Contrail.prototype.positionHistory = [];
 
-Contrail.prototype.reset = function (objectPosition, objectRotation, objectRoll, objectVelocity) {
-    this.positionHistory = [];
-    for (var i = this.length; i > 0; i--) {
-        this.positionHistory[this.length - i] = [objectPosition[0], objectPosition[1], objectRotation, objectRoll, objectVelocity[0], objectVelocity[1]];
-    }
-    this.update(objectPosition, objectRotation, objectRoll, objectVelocity);
-    // console.log(JSON.parse(JSON.stringify(this.positionHistory)));
+Contrail.prototype.reset = function(objectPosition, objectRotation, objectRoll, objectVelocity) {
+  this.positionHistory = [];
+  for (var i = this.length; i > 0; i--) {
+    this.positionHistory[this.length - i] = [objectPosition[0], objectPosition[1], objectRotation, objectRoll, objectVelocity[0], objectVelocity[1]];
+  }
+  this.update(objectPosition, objectRotation, objectRoll, objectVelocity);
+  // console.log(JSON.parse(JSON.stringify(this.positionHistory)));
 };
 
-Contrail.prototype.update = function (objectPosition, objectRotation, objectRoll, objectVelocity) {
+Contrail.prototype.update = function(objectPosition, objectRotation, objectRoll, objectVelocity) {
+  objectRoll -= Math.PI / 2;
 
-    objectRoll -= Math.PI / 2;
+  var u = this.material.uniforms;
+  u.objectPosition.value.x = objectPosition[0];
+  u.objectPosition.value.y = objectPosition[1];
+  u.objectRotation.value = objectRotation;
+  u.objectRoll.value = objectRoll;
+  u.objectVelocity.value.x = objectVelocity[0];
+  u.objectVelocity.value.y = objectVelocity[1];
 
-    var u = this.material.uniforms;
-    u.objectPosition.value.x = objectPosition[0];
-    u.objectPosition.value.y = objectPosition[1];
-    u.objectRotation.value = objectRotation;
-    u.objectRoll.value = objectRoll;
-    u.objectVelocity.value.x = objectVelocity[0];
-    u.objectVelocity.value.y = objectVelocity[1];
+  var objectPositions = this.geometry.attributes.objectPositions.array;
+  var objectVelocities = this.geometry.attributes.objectVelocities.array;
+  var objectRotations = this.geometry.attributes.objectRotations.array;
+  var objectRolls = this.geometry.attributes.objectRolls.array;
 
-    var objectPositions = this.geometry.attributes.objectPositions.array;
-    var objectVelocities = this.geometry.attributes.objectVelocities.array;
-    var objectRotations = this.geometry.attributes.objectRotations.array;
-    var objectRolls = this.geometry.attributes.objectRolls.array;
+  this.positionHistory[this.length] = [objectPosition[0], objectPosition[1], objectRotation, objectRoll, objectVelocity[0], objectVelocity[1]];
+  for (var i = 1, il = this.length; i <= il; i++) {
+    this.positionHistory[i - 1] = this.positionHistory[i];
+  }
 
+  for (var i = 0, index, index2, j, il = parseInt(this.length / this.break); i < il; i++) {
+    j = (i + 1) * this.break;
 
-    this.positionHistory[this.length] = [objectPosition[0], objectPosition[1], objectRotation, objectRoll, objectVelocity[0], objectVelocity[1]];
-    for (var i = 1, il = this.length; i <= il; i++) {
-        this.positionHistory[i - 1] = this.positionHistory[i];
-    }
+    index = i * 3;
+    index2 = (i + il) * 3;
 
-    for (var i = 0, index, index2, j, il = parseInt(this.length / this.break); i < il; i++) {
+    objectPositions[index] = this.positionHistory[j][0];
+    objectPositions[index + 1] = this.positionHistory[j][1];
+    objectPositions[index + 2] = 1;
+    objectPositions[index2] = this.positionHistory[j][0];
+    objectPositions[index2 + 1] = this.positionHistory[j][1];
+    objectPositions[index2 + 2] = -1;
 
-        j = (i + 1) * this.break;
+    objectVelocities[index] = this.positionHistory[j][4];
+    objectVelocities[index + 1] = this.positionHistory[j][5];
+    objectVelocities[index + 2] = 0;
+    objectVelocities[index2] = this.positionHistory[j][4];
+    objectVelocities[index2 + 1] = this.positionHistory[j][5];
+    objectVelocities[index2 + 2] = 0;
 
-        index = i * 3;
-        index2 = ( i + il ) * 3;
+    index = i;
+    index2 = i + il;
 
-        objectPositions[index] = this.positionHistory[j][0];
-        objectPositions[index + 1] = this.positionHistory[j][1];
-        objectPositions[index + 2] = 1;
-        objectPositions[index2] = this.positionHistory[j][0];
-        objectPositions[index2 + 1] = this.positionHistory[j][1];
-        objectPositions[index2 + 2] = -1;
+    objectRotations[index] = this.positionHistory[j][2];
+    objectRotations[index2] = this.positionHistory[j][2];
 
-        objectVelocities[index] = this.positionHistory[j][4];
-        objectVelocities[index + 1] = this.positionHistory[j][5];
-        objectVelocities[index + 2] = 0;
-        objectVelocities[index2] = this.positionHistory[j][4];
-        objectVelocities[index2 + 1] = this.positionHistory[j][5];
-        objectVelocities[index2 + 2] = 0;
+    objectRolls[index] = this.positionHistory[j][3];
+    objectRolls[index2] = this.positionHistory[j][3];
+  }
 
-        index = i;
-        index2 = i + il;
-
-        objectRotations[index] = this.positionHistory[j][2];
-        objectRotations[index2] = this.positionHistory[j][2];
-
-        objectRolls[index] = this.positionHistory[j][3];
-        objectRolls[index2] = this.positionHistory[j][3];
-
-    }
-
-    this.geometry.attributes.objectPositions.needsUpdate = true;
-    this.geometry.attributes.objectVelocities.needsUpdate = true;
-    this.geometry.attributes.objectRotations.needsUpdate = true;
-    this.geometry.attributes.objectRolls.needsUpdate = true;
-
+  this.geometry.attributes.objectPositions.needsUpdate = true;
+  this.geometry.attributes.objectVelocities.needsUpdate = true;
+  this.geometry.attributes.objectRotations.needsUpdate = true;
+  this.geometry.attributes.objectRolls.needsUpdate = true;
 };
 
 export default Contrail;
--- src/states/main/gameObjects/weapons/projectileRenderer.js
+++ src/states/main/gameObjects/weapons/projectileRenderer.js
@@ -1,92 +1,89 @@
-import {AdditiveBlending, BufferAttribute, BufferGeometry, Points, ShaderMaterial, Vector3} from "three";
+import { AdditiveBlending, BufferAttribute, BufferGeometry, Points, ShaderMaterial, Vector3 } from "three";
 
-var ProjectileRenderer = function (weapon) {
+var ProjectileRenderer = function(weapon) {
+  weapon.size = weapon.size || 32;
 
-    weapon.size = weapon.size || 32;
+  this.weapon = weapon;
 
-    this.weapon = weapon;
+  var pointCount = weapon.projectileBufferSize;
+  var positionsBuffer = new Float32Array(pointCount * 3);
 
-    var pointCount = weapon.projectileBufferSize;
-    var positionsBuffer = new Float32Array(pointCount * 3);
+  for (var i = 0; i < pointCount; i++) {
+    var pos = new Vector3(i * 10, 0, 0);
+    pos.toArray(positionsBuffer, i * 3);
+  }
 
-    for (var i = 0; i < pointCount; i++) {
-        var pos = new Vector3(i * 10, 0, 0);
-        pos.toArray(positionsBuffer, i * 3);
-    }
+  var geometry = new BufferGeometry();
+  geometry.setAttribute("position", new BufferAttribute(positionsBuffer, 3));
 
-    var geometry = new BufferGeometry();
-    geometry.addAttribute('position', new BufferAttribute(positionsBuffer, 3));
+  var vertexshader = [
+    "uniform float elapsedTime;",
+    "void main() {",
 
-    var vertexshader = [
-        'uniform float elapsedTime;',
-        'void main() {',
+    "vec4 mvPosition = modelViewMatrix * vec4( position.x, position.y, position.z , 1.0 );",
 
-        'vec4 mvPosition = modelViewMatrix * vec4( position.x, position.y, position.z , 1.0 );',
+    "gl_PointSize = " + weapon.size + ".0;",
+    "gl_Position = projectionMatrix * mvPosition;",
 
-        'gl_PointSize = ' + weapon.size + '.0;',
-        'gl_Position = projectionMatrix * mvPosition;',
+    "}",
 
-        '}',
+    ""
+  ].join("\n");
 
-        ''].join('\n');
+  var fragmentshader = [
+    "uniform sampler2D texture;",
+    "uniform vec2 textureCrop;",
+    "void main() {",
 
-    var fragmentshader = [
-        'uniform sampler2D texture;',
-        'uniform vec2 textureCrop;',
-        'void main() {',
+    "vec2 vUv = vec2( gl_PointCoord.x, 1.0 - gl_PointCoord.y );",
 
-        'vec2 vUv = vec2( gl_PointCoord.x, 1.0 - gl_PointCoord.y );',
+    "vUv.x = textureCrop.x - (gl_PointCoord.x * 0.25);",
+    "vUv.y = textureCrop.y - (gl_PointCoord.y * 0.25);",
 
-        'vUv.x = textureCrop.x - (gl_PointCoord.x * 0.25);',
-        'vUv.y = textureCrop.y - (gl_PointCoord.y * 0.25);',
+    "vec4 textureColor = texture2D( texture, vUv);",
 
-        'vec4 textureColor = texture2D( texture, vUv);',
+    "gl_FragColor = textureColor;",
+    "}"
+  ].join("\n");
 
-        'gl_FragColor = textureColor;',
-        '}'
-    ].join('\n');
+  var material = new ShaderMaterial({
+    uniforms: {
+      texture: { type: "t", value: weapon.texture },
+      elapsedTime: { type: "f", value: 0 },
+      textureCrop: { type: "v2", value: weapon.textureCrop }
+    },
+    vertexShader: vertexshader,
+    fragmentShader: fragmentshader,
+    transparent: true,
+    depthTest: true,
+    depthWrite: false,
+    blending: AdditiveBlending
+  });
 
-    var material = new ShaderMaterial({
-        uniforms: {
-            texture: {type: "t", value: weapon.texture},
-            elapsedTime: {type: 'f', value: 0},
-            textureCrop: {type: 'v2', value: weapon.textureCrop}
-        },
-        vertexShader: vertexshader,
-        fragmentShader: fragmentshader,
-        transparent: true,
-        depthTest: true,
-        depthWrite: false,
-        blending: AdditiveBlending
-    });
+  Points.call(this, geometry, material);
 
-    Points.call(this, geometry, material);
-
-    this.frustumCulled = false;
-
+  this.frustumCulled = false;
 };
 
 ProjectileRenderer.prototype = Object.create(Points.prototype);
 ProjectileRenderer.prototype.constructor = ProjectileRenderer;
 
-ProjectileRenderer.prototype.update = function () {
+ProjectileRenderer.prototype.update = function() {
+  var w = this.weapon;
 
-    var w = this.weapon;
-
-    for (var i = 0, index = 0, p; i < w.projectileBufferSize; i++) {
-        p = w.projectileBuffer[i];
-        if (p.visible) {
-            p.update();
-            index = i * 3;
-            this.geometry.attributes.position.array[index] = p.body.position[0];
-            this.geometry.attributes.position.array[index + 1] = p.body.position[1];
-            this.geometry.attributes.position.array[index + 2] = 0;
-        } else {
-            this.geometry.attributes.position.array[i * 3 + 2] = -3000;
-        }
+  for (var i = 0, index = 0, p; i < w.projectileBufferSize; i++) {
+    p = w.projectileBuffer[i];
+    if (p.visible) {
+      p.update();
+      index = i * 3;
+      this.geometry.attributes.position.array[index] = p.body.position[0];
+      this.geometry.attributes.position.array[index + 1] = p.body.position[1];
+      this.geometry.attributes.position.array[index + 2] = 0;
+    } else {
+      this.geometry.attributes.position.array[i * 3 + 2] = -3000;
     }
-    this.geometry.attributes.position.needsUpdate = true;
-
+  }
+  this.geometry.attributes.position.needsUpdate = true;
 };
 
 export default ProjectileRenderer;
